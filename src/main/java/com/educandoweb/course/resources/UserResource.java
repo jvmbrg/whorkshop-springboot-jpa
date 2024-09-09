34bb153cb6ba51ac0c6e9feff90d6e76837d4d8f
jvmbrg <jvitormbraga@gmail.com>
2024-09-09 20:21:50 -0300

    Service layer, component registration

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -1,25 +1,41 @@
 package com.educandoweb.course.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 //Pacote de resoluções web, responsavel por fazer as requisições referentes ao User.
 
 import com.educandoweb.course.entities.User;
+import com.educandoweb.course.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users") 
 /* Aqui o "/users" é o caminho que vai exibir as informações do metodo findAll. Quando dermos o localhost:8080/users, será exibido um json com as informa
  * ções passadas por parametro dentro do metodo.*/
+
+
 public class UserResource {
+	@Autowired
+	private UserService service;
 	
 	@GetMapping //Esse método é um endpoint para acessar os usuarios.
 	//O ResponseEntity é do tipo generico, por isso, precisamos definir qual o tipo do retorno.
-	public ResponseEntity<User> findAll(){
-		User u = new User(1L, "Maria", "maria@gmail.com", "71982166003", "123456");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll(){
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
 		//Esse tipo de retorno ResponseEntity é especifico para retornar respostas de requisição WEB.
 	}
+	
+	@GetMapping(value="/{id}")
+	public ResponseEntity <User> findById(@PathVariable Long id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
 
 }
---
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -0,0 +1,32 @@
+package com.educandoweb.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.educandoweb.course.entities.User;
+import com.educandoweb.course.repositories.UserRepository;
+/* Aqui no pacote serviços temos a classe UserService que é responsavel por gerir os servi
+ * ços dos usuarios. Atraves do sistema de camadas de funções, essa classe é a que fica encarre
+ * gada de receber as solicitações do controller e fazer a busca dentro do banco de dados
+ * (repository), como se fosse o meio termo entre elas.
+ */
+
+@Service
+public class UserService {
+	@Autowired
+	private UserRepository repository;
+	//Metodo responsavel por repassar a chamada do metodo findAll para a camada repository.
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	/* Meteodo reponsavel por repassar a chamada do metodo findById para a camada repository
+	*  Aqui, o metodo retorna um usuario com base no id do cadastro.*/
+	public User findById(Long id) {
+		//O comando "Optional e representa valores que podem ou não estar estar presentes na chamada, evitando erro de NullPointException" 
+		Optional<User> obj = repository.findById(id);
+		return obj.get(); // o metodo get retorna um objeto do tipo User que estiver dentro do Optional.
+	}
+}
