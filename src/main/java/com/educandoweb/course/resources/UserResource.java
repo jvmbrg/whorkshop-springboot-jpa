dde0a490aff56fcc4678f038f3fcc3024ff2a8f3
jvmbrg <jvitormbraga@gmail.com>
2024-09-09 17:25:11 -0300

    H2 database, test profile, JPA

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -9,14 +9,17 @@ import org.springframework.web.bind.annotation.RestController;
 import com.educandoweb.course.entities.User;
 
 @RestController
-@RequestMapping(value = "/users")
+@RequestMapping(value = "/users") 
+/* Aqui o "/users" é o caminho que vai exibir as informações do metodo findAll. Quando dermos o localhost:8080/users, será exibido um json com as informa
+ * ções passadas por parametro dentro do metodo.*/
 public class UserResource {
 	
-	@GetMapping
+	@GetMapping //Esse método é um endpoint para acessar os usuarios.
+	//O ResponseEntity é do tipo generico, por isso, precisamos definir qual o tipo do retorno.
 	public ResponseEntity<User> findAll(){
 		User u = new User(1L, "Maria", "maria@gmail.com", "71982166003", "123456");
 		return ResponseEntity.ok().body(u);
-		
+		//Esse tipo de retorno ResponseEntity é especifico para retornar respostas de requisição WEB.
 	}
 
 }
