43deff7543fc255279d6b72ee2cd8a6664b19b92
jvmbrg <jvitormbraga@gmail.com>
2024-09-11 10:31:22 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/educandoweb/course/entities/Product.java
+++ src/main/java/com/educandoweb/course/entities/Product.java
@@ -5,12 +5,19 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
-import jakarta.persistence.Transient;
+
 @Entity
 @Table(name = "tb_product")
 public class Product implements Serializable{
@@ -24,7 +31,10 @@ public class Product implements Serializable{
 	private Double price;
 	private String imgUrl;
 	
-	@Transient //para impedir o JPA de tentar interpretar essa lista
+	@ManyToMany @Fetch(FetchMode.JOIN)// Associação muitos pra muitos
+	@JoinTable(name="tb_product_category", //Criação da tabela de associação entre as tabelas category e product
+	joinColumns=@JoinColumn(name="product_id"), //Criação da  chave estrangeira referente a tabela produto
+	inverseJoinColumns = @JoinColumn(name="category_id")) //Criação da chave estrangeira referente a tabela category
 	private Set <Category> categories = new HashSet<>(); //Usado pra garantir que não tenha um produto com duas categorias diferentes
 	
 	public Product() {}
