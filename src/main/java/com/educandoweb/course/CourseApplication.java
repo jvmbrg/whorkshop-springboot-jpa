023ab65955a3868d5de5cde61e1769a9ac97c0e4
jvmbrg <jvitormbraga@gmail.com>
2024-09-19 09:08:16 -0300

    Update application.prod and .properties

--- src/main/java/com/educandoweb/course/CourseApplication.java
+++ src/main/java/com/educandoweb/course/CourseApplication.java
@@ -1,13 +1,20 @@
 package com.educandoweb.course;
 
+import java.util.Map;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
 public class CourseApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CourseApplication.class, args);
+		
+		
 	}
-
+	
+	
 }
